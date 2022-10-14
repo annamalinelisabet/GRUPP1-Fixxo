68bb181ebda7d1ad236d9540bfd58268f6263996
Lisono <linnor74@gmail.com>
2022-10-14 10:47:14 +0200

    first commit Hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Hero.css'
+
+
+const Hero = () => {
+  return (
+    <div className='mt-3 hero-bg hero-row d-flex align-items-center'>
+      
+      <div className='col h-100 d-none d-sm-block hero-bg-left'>
+      </div>
+      
+      <div className='col h-100 hero-align-col text-center'>
+          <p className='hero-header'>SALE UP <br></br>To 50% Of</p>
+          <p className='hero-text'>Online shopping free home delivery over $100</p>
+          <button className='btn-hero'>SHOP NOW</button>
+      </div>
+      
+      <div className='col h-100 d-none d-lg-block hero-bg-right'>
+      </div>
+
+    </div>
+  )
+}
+
+export default Hero
--- src/views/Home/HomeView.jsx
+++ src/views/Home/HomeView.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Hero from '../../components/Hero/Hero'
 import './HomeView.css'
 
 const Home = () => {
   return (
     <div className='Home'>
       Home
+      <div>
+        <Hero />
+      </div>
     </div>
   )
 }
