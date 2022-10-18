6c318d7463a6c086200ace53228f9893e17f2523
Emma Wibeck <Emmawibeck@gmail.com>
2022-10-18 18:00:41 +0200

    lagt till det mesta för news

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import Views from './views/Views'
+import News from './components/News/News';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Views />
+      <News />
     </div>
   );
 }
