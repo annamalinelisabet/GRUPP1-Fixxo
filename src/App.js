febefd24ce351ee1aaeae213f1944d8497ab7336
SimonFEU21 <simon.habchi@utb.ecutbildning.se>
2022-10-19 00:32:25 +0200

    support och info utseende färdigt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
+import SupportInfo from './components/Support&Info/SupportInfo'
 import Views from './views/Views'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Views />
+      <SupportInfo />
     </div>
   );
 }
---
+++ src/components/Support&Info/SupportInfo.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import './SupportInfo.css'
+
+
+
+function SupportInfo() {
+  return (
+
+      
+      <div className="suppInfo-icons">
+
+          {/* <div className='bg-image'>
+          <img src="images/RectangleBG.png" alt="background" />
+         </div> */}
+
+        <div className='suppIcon-1'>
+        <img src="images/logo1.png" alt="customer-support" />
+        <h3 className='icon-text'>Customer Support</h3>
+        <p>Village did removed enjoyed <br /> explain talking.</p>
+        </div>
+
+        <div className='suppIcon-2'>
+        <img src="images/logo2.png" alt="secured-payment" />
+        <h3 className='icon-text'>Secured Payment</h3>
+        <p>Village did removed enjoyed <br /> explain talking.</p>
+        </div>
+
+        <div className='suppIcon-3'>
+        <img src="images/logo3.png" alt="free-home-delivery" />
+        <h3 className='icon-text'>Free Home Delivery</h3>
+        <p>Village did removed enjoyed <br /> explain talking.</p>
+        </div>
+
+        <div className='suppIcon-4'>
+        <img src="images/logo4.png" alt="30-day-reuters" />
+        <h3 className='icon-text'>30 Day Reuters</h3>
+        <p>Village did removed enjoyed <br /> explain talking.</p>
+        </div>
+
+    </div>
+
+
+
+
+
+  )
+}
+
+export default SupportInfo
