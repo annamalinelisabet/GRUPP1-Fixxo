9928d636056a8db99c227f224495d4e9ceeed70c
Emma Wibeck <Emmawibeck@gmail.com>
2022-10-19 11:13:35 +0200

    ändrat

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import Views from './views/Views'
-import News from './components/News/News';
-import Inspiration from './components/Inspiration/Inspiration';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Views />
-      <News />
-      <Inspiration />
     </div>
   );
 }
--- src/views/Home/HomeView.jsx
+++ src/views/Home/HomeView.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Footer from '../../components/Footer/Footer'
 import Hero from '../../components/Hero/Hero'
 import './HomeView.css'
+import News from '../../components/News/News';
+import Inspiration from '../../components/Inspiration/Inspiration'
+
 
 const Home = () => {
   return (
     <div className='Home'>
       <div>
         <Hero />
+        <News />
+        <Inspiration />
       </div>
       <div>
         <Footer />
