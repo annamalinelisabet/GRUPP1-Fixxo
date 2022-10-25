34c99227890868e15272c416c75e0a3d2aab7832
Lisono <linnor74@gmail.com>
2022-10-25 21:42:54 +0200

    design filter functions

--- src/views/ProductsLinkView/ProductsLinkView.jsx
+++ src/views/ProductsLinkView/ProductsLinkView.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProductCardCentered from '../../components/Products/ProductCardCentered';
-import "./ProductsLinkView.css";
+import './ProductsLinkView.css';
 
 
 const ProductsLinkView = () => {
@@ -34,12 +34,24 @@ const ProductsLinkView = () => {
         <div className='pt-3 d-flex'> 
             {/*cat menu */}
             <div className='col-3'>
-                <ul class="list-group list-group-light list-group-small">
-                    <li class="list-group-item">All Categories</li>
-                    <li class="list-group-item">Price</li>
-                    <li class="list-group-item">Size</li>
-                    <li class="list-group-item disabled">Color</li>
-                    <li class="list-group-item disabled">Brands</li>
+                <ul className="list-group list-group-light list-group-small">
+                    <li className="list-group-item">All Categories</li>
+                        <ul className='sublist'>
+                            <li className='sublist-item'>T-shirts</li>
+                            <li className='sublist-item'>Pants</li>
+                            <li className='sublist-item'>Jackets</li>
+                        </ul>
+                    <li className="list-group-item">Price</li>
+
+                    <li className="list-group-item">Size</li>
+                        <ul className='sublist'>
+                            <li className='sublist-item'>Small</li>
+                            <li className='sublist-item'>Medium</li>
+                            <li className='sublist-item'>Large</li>
+                        </ul>
+                    <li className="list-group-item disabled">Color</li>
+                    <li className="list-group-item disabled">Brands</li>
+                    
                 </ul>
             </div>
             {/*End cat menu */}
@@ -49,13 +61,49 @@ const ProductsLinkView = () => {
             <div className='col-9'>
                 {/* filter functions */}
                 <div className='d-flex justify-content-between'>
-                    <p className='border'>Filter list selections</p>
-                    <p className='border'>Show # selection</p>
+                  {/*Sort by function  */}
+                  <div className=''>
+                    <button type="button" className="btn-list btn-left">Most Popular</button>
+                    <button
+                        type="button"
+                        className="btn-list btn-right dropdown-toggle dropdown-toggle-split"
+                        data-mdb-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        <span className="visually-hidden">Toggle Dropdown</span>
+                    </button>
+                    <ul className='dropdown-menu dropdown-menu-end'>
+                        <li><a className='dropdown-item' href="#">Price Lowest </a></li>
+                        <li><a className='dropdown-item' href="#">Price Highest</a></li>
+                        <li><a className='dropdown-item' href="#">Alphabetical</a></li>
+                        <li><a className='dropdown-item' href="#">Reviews</a></li>
+                    </ul>
+                  </div>
+
+                    {/* Show # of products function */}
+                  <div className=''>
+                    <small>Show: </small>
+                    <button type="button" className="btn-list btn-left btn-small">12</button>
+
+                    <button
+                        type="button"
+                        className="btn-list btn-right dropdown-toggle dropdown-toggle-split"
+                        data-mdb-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        <span className="visually-hidden">Toggle Dropdown</span>
+                    </button>
+                    <ul className='dropdown-menu dropdown-menu-end text-end'>
+                        <li><a className='dropdown-item' href="#">24 </a></li>
+                        <li><a className='dropdown-item' href="#">50</a></li>
+                        <li><a className='dropdown-item' href="#">100</a></li>
+                    </ul>
+                  </div>
+
                 </div>
 
                 {/*products grid, 3col 4row */}
-                <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-2 mt-2'>
-                    
+                <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-2 mt-2'>        
                     <ProductCardCentered />
                     <ProductCardCentered />
                     <ProductCardCentered />
@@ -63,39 +111,32 @@ const ProductsLinkView = () => {
                     <ProductCardCentered />
                     <ProductCardCentered />
                     <ProductCardCentered />
-
-
-
-
                 </div>
-
-                {/* Pagination nav */}
-                <nav aria-label="...">
-                    <ul className="pagination pagination-circle">
-                        <li className="page-item">
-                            <a className="page-link"><i className="fa-solid fa-angle-left"></i></a>
-                        </li>
-                        <li className="page-item">
-                            <a className="page-link" href="#">1</a>
-                        </li>
-                        <li className="page-item active" aria-current="page">
-                            <a className="page-link" href="#">2 <span className="visually-hidden">(current)</span></a>
-                        </li>
-                        <li className="page-item">
-                            <a className="page-link" href="#">3</a>
-                        </li>
-                        <li className="page-item">
-                            <a className="page-link" href="#"><i className="fa-solid fa-angle-right"></i></a>
-                        </li>
-                    </ul>
-                </nav>
-                {/* End Pagination nav */}
-
             </div>
             {/*End products grid */}
-
-
         </div>  
+
+            {/* Pagination nav */}
+            <nav aria-label="pagination more products" className='mt-3 d-flex justify-content-center'>
+                <ul className="pagination pagination-circle">
+                    <li className="page-item">
+                        <a className="page-link"><i className="fa-solid fa-angle-left"></i></a>
+                    </li>
+                    <li className="page-item active">
+                        <a className="page-link" href="#">01 <span className="visually-hidden">(current)</span></a>
+                    </li>
+                    <li className="page-item" aria-current="page">
+                        <a className="page-link" href="#">02</a>
+                    </li>
+                    <li className="page-item">
+                        <a className="page-link" href="#">03</a>
+                    </li>
+                    <li className="page-item">
+                        <a className="page-link" href="#"><i className="fa-solid fa-angle-right"></i></a>
+                    </li>
+                </ul>
+            </nav>
+            {/* End Pagination nav */}
     
     </div>
   )
