a7d59c2b79cbf74a60e03774748065cbae65a2e2
Malin Andersson <annamalinelisabet@gmail.com>
2022-10-27 09:47:51 +0200

    checkout sida

---
+++ src/components/CheckoutTop/CheckoutTop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './CheckoutTop.css'
+
+const CheckoutTop = () => {
+  return (
+    <div className='m-CheckoutTop'>
+        <h4>Not a member yet? Join here for free!</h4>
+        <p>Become a member — don’t miss out on deals, offers, discounts and bonus vouchers.</p>
+        <button className='btn-hero'>JOIN OR LOGIN</button>
+    </div>
+  )
+}
+
+export default CheckoutTop
--- src/views/Views.jsx
+++ src/views/Views.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Checkout from './Checkout/Checkout'
 import HomeView from './Home/HomeView'
 
 const Views = () => {
   return (
     <Routes>
         <Route path ="/" element={ <HomeView /> } />
+        <Route path ="/checkout" element={ <Checkout /> } />
     </Routes>
   )
 }
