087256308cd6597e2aa37c9cdceb245b6a65fd2c
Emma Wibeck <Emmawibeck@gmail.com>
2022-10-27 13:03:18 +0200

    småfix klart

--- src/components/Speciality/Speciality.jsx
+++ src/components/Speciality/Speciality.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Speciality.css'
 import '../../App.css'
 import woman from '../../images/woman.svg'
@@ -12,9 +13,9 @@ const Speciality = () => {
       <div class="row d-flex justify-content-center align-items-center flex-md-column flex-lg-row">
 
         <div class="col-lg-4 col-md-10 d-flex min-height mb-lg-0 mb-2">
-          <div className='col-6 textBackground p-2 d-flex flex-column justify-content-center ps-3'>
+          <div className='col-6 textBackground p-2 d-flex flex-column justify-content-center ps-3 custom-card'>
             <p>Track Your Order</p>
-            <p className='text-decoration-underline cursor-pointer'>Get Started<i class="fas fa-angle-right"></i></p>
+             <Link to="/"><p className='text-decoration-underline cursor-pointer'>Get Started<i class="fas fa-angle-right"></i></p></Link>
           </div>
           <div className='col-6 grey p-2 d-flex justify-content-center align-items-center'>
             <img src={woman} className="imgPic" alt=".." />
@@ -22,9 +23,9 @@ const Speciality = () => {
         </div>
 
         <div class="col-lg-4 col-md-10 d-flex min-height mb-lg-0 mb-2">
-          <div className='col-6 textBackground p-2 d-flex flex-column justify-content-center ps-3'>
+          <div className='col-6 textBackground p-2 d-flex flex-column justify-content-center ps-3 custom-card'>
             <p>Make a Return</p>
-            <p className='text-decoration-underline'>Get Started <i class="fas fa-angle-right"></i></p>
+            <Link to="/"><p className='text-decoration-underline'>Get Started <i class="fas fa-angle-right"></i></p></Link>
           </div>
           <div className='col-6 grey p-2 d-flex justify-content-center align-items-center'>
             <img src={woman} className="imgPic" alt=".." />
@@ -32,9 +33,9 @@ const Speciality = () => {
         </div>
 
         <div class="col-lg-4 col-md-10 d-flex min-height">
-          <div className='col-6 textBackground p-2 d-flex flex-column justify-content-center ps-3'>
+          <div className='col-6 textBackground p-2 d-flex flex-column justify-content-center ps-3 custom-card'>
             <p>Request a Price Adjustment</p>
-            <p className='text-decoration-underline'>Get Started <i class="fas fa-angle-right"></i></p>
+            <Link to="/"><p className='text-decoration-underline'>Get Started <i class="fas fa-angle-right"></i></p></Link>
           </div>
           <div className='col-6 grey p-2 d-flex justify-content-center align-items-center'>
             <img src={woman} className="imgPic" alt=".." />
