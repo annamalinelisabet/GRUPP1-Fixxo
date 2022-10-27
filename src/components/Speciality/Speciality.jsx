bf32be49e65ade0e8240b9379faddedefe104903
Emma Wibeck <Emmawibeck@gmail.com>
2022-10-27 09:44:44 +0200

    påbörjat med our speciality

---
+++ src/components/Speciality/Speciality.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Speciality.css'
+
+const Speciality = () => {
+  return (
+    <div class="container  bg mt-4">
+      <h6 className='text-center'> Our Speciality </h6>
+      <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-lg-4 col-md-6 d-flex">
+          <div className='bg2 p-2'>
+            <p>Track Your Order</p>
+            <p>Get Started</p>
+          </div>
+          <div className='bg12 p-2'>CARD</div>
+        </div>
+        <div class="col-lg-4 col-md-6 d-flex">
+          <div className='bg2 p-2'>
+            <p>Track Your Order</p>
+            <p>Get Started</p>
+          </div>
+          <div className='bg12 p-2'>CARD</div>
+        </div>
+        <div class="col-lg-4 col-md-6 d-flex">
+          <div className='bg2 p-2'>
+            <p>Track Your Order</p>
+            <p>Get Started</p>
+          </div>
+          <div className='bg12 p-2'>CARD</div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Speciality
