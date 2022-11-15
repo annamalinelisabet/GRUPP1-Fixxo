ac110cc80ad032c3d7d04537202308443beb223b
AnasZantoutt <anaszantouttt@gmail.com>
2022-11-15 12:27:38 +0100

    productlist

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,36 +1,43 @@
 import React from "react";
 import "./products.css";
+import { Link } from 'react-router-dom'
 
-
-
-
-import bg from "./img/bg.svg";
 import stars from "./img/stars.svg";
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
+
+
+  
+
+
   return (
-   
+     
+    <Link to={`/products/${product.id}`}>
+
+      
+    
     <div className="col">
       <div className="">
         <div className="row">
           <div className="col-6">
-            <img src={bg} alt="img" className="card-img" />
+          
+            <img src={product.image} alt="img" className="card-img" />
           </div>
           <div className="col-6">
             <p className="category-text">
               <small className="">Category</small>
             </p>
-            <h5 className="title-text">Modern Black Blouse</h5>
+            <h5 className="title-text">{product.name}</h5>
             <img src={stars} alt="stars" className="stars" />
             <p className="price-text">
-              <b>$35.00</b>
+              <b>{product.name}</b>
             </p>
             
           </div>
         </div>
       </div>
     </div>
-    
+    </Link>
   );
 };
 
--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -1,47 +1,56 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 import "./products.css";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts } from '../../store/actions/productsActions';
+
+
 
 const ProductList = () => {
+    
+  const dispatch = useDispatch()
+
+    const {data: products, loading} = useSelector(state => state.products)
+
+    useEffect(() => {
+        dispatch(getProducts())
+    }, [dispatch])
+
   return (
-    <section className="product-container mt-5">
-      <div className="product-wrapper">
-        <h4>Latest Product</h4>
-        <div className="product-card mt-4">
-          <ProductCard />
-        </div>
-        <div className="product-card">
-          <ProductCard />
-        </div>
-        <div className="product-card">
-          <ProductCard />
-        </div>
-      </div>
-      <div className="product-wrapper">
-        <h4>Best Selling Product</h4>
-        <div className="product-card mt-4">
-          <ProductCard />
-        </div>
-        <div className="product-card">
-          <ProductCard />
-        </div>
-        <div className="product-card">
-          <ProductCard />
-        </div>
-      </div>
-      <div className="product-wrapper">
-        <h4>Top Reacted Product</h4>
-        <div className="product-card mt-4">
-          <ProductCard />
-        </div>
-        <div className="product-card">
-          <ProductCard />
-        </div>
-        <div className="product-card">
-          <ProductCard />
-        </div>
-      </div>
-    </section>
+    <div>
+
+    
+                      
+                    { loading && <p>Loading...</p> }
+                      { products.slice(0,3).map(product => (
+        <section className="product-container mt-5">
+                      
+        
+          <div className="product-wrapper">
+            <h4>Latest Product</h4>
+            <div className="product-card mt-4">
+            <ProductCard key={product.id} product={product}/> 
+            </div>
+          </div>
+
+          <div className="product-wrapper">
+            <h4>Best Selling Product</h4>
+            <div className="product-card mt-4">
+            <ProductCard key={product.id} product={product}/> 
+            </div>
+            
+          </div>
+          <div className="product-wrapper">
+            <h4>Top Reacted Product</h4>
+            <div className="product-card mt-4">
+            <ProductCard key={product.id} product={product}/> 
+            </div>
+            
+          </div>
+        </section>
+      ))}  
+    </div>
   );
 };
 
