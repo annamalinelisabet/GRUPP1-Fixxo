81095874ec8ddd8edd073c0af03b3f3e6905edae
Emma Wibeck <Emmawibeck@gmail.com>
2022-11-17 14:10:58 +0100

    lagt till kod

--- src/components/Support&Info/SupportInfo.jsx
+++ src/components/Support&Info/SupportInfo.jsx
@@ -7,31 +7,35 @@ import './SupportInfo.css'
 function SupportInfo() {
   return (
 
-      <div className="suppInfo-icons">
+      <div className=" container d-flex flex-column justify-content-center align-items-center d-md-flex flex-md-row mb-md-4">
+        
+        <div className='f-md-flex flex-md-column me-md-5 me-lg-0 d-lg-flex flex-lg-row '>
+            <div className='suppIcon pe-lg-5'>
+              <img src="images/logo1.png" alt="customer-support" />
+              <h4 className='icon-text'>Customer Support</h4>
+              <p>Village did removed enjoyed <br /> explain talking.</p>
+            </div>
 
-        <div className='suppIcon-1'>
-        <img src="images/logo1.png" alt="customer-support" />
-        <h3 className='icon-text'>Customer Support</h3>
-        <p>Village did removed enjoyed <br /> explain talking.</p>
+            <div className='suppIcon pe-lg-5'>
+              <img src="images/logo2.png" alt="secured-payment" />
+              <h4 className='icon-text'>Secured Payment</h4>
+              <p>Village did removed enjoyed <br /> explain talking.</p>
+            </div>
         </div>
 
-        <div className='suppIcon-2'>
-        <img src="images/logo2.png" alt="secured-payment" />
-        <h3 className='icon-text'>Secured Payment</h3>
-        <p>Village did removed enjoyed <br /> explain talking.</p>
-        </div>
+        <div className='f-md-flex flex-md-column d-lg-flex flex-lg-row'>
+            <div className='suppIcon pe-lg-5'>
+              <img src="images/logo3.png" alt="free-home-delivery" />
+              <h4 className='icon-text'>Free Home Delivery</h4>
+              <p>Village did removed enjoyed <br /> explain talking.</p>
+            </div>
 
-        <div className='suppIcon-3'>
-        <img src="images/logo3.png" alt="free-home-delivery" />
-        <h3 className='icon-text'>Free Home Delivery</h3>
-        <p>Village did removed enjoyed <br /> explain talking.</p>
-        </div>
-
-        <div className='suppIcon-4'>
-        <img src="images/logo4.png" alt="30-day-reuters" />
-        <h3 className='icon-text'>30 Day Reuters</h3>
-        <p>Village did removed enjoyed <br /> explain talking.</p>
-        </div>
+            <div className='suppIcon mb-3 mb-md-0'>
+              <img src="images/logo4.png" alt="30-day-reuters" />
+              <h4 className='icon-text'>30 Day Returns</h4>
+              <p>Village did removed enjoyed <br /> explain talking.</p>
+            </div>
+          </div>
 
     </div>
 
